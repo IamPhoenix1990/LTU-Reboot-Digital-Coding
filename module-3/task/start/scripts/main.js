70558c104a6df401065be2a27010bfbb142a2b89
Zac <69262818+IamPhoenix1990@users.noreply.github.com>
2021-06-21 21:01:13 +0100

    Javascript basics and commenting

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,29 @@
+console.log('test');
+
+// Single line comment 
+
+/*
+
+Multiline coment
+
+*/
+
+//List of things that are "false"
+let strVariable = '';
+let numberOfKittens = 0;
+let blnVariable = false;
+let arrVariable = [];
+let objVariable = {};
+let variable; //undefined
+let emptyVar = null;
+// everything else is true!
+
+
+let strProductName = 'Pasta';
+let price = 1.99;
+let quantity = 5;
+let inStock = true;
+let discountAmount = 0;
+
+console.log(strProductName);
+console.log(price);
