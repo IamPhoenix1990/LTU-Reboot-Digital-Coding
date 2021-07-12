e8f7217e9ec1e7bb7534fa5357f8fd78197a9299
Zac <69262818+IamPhoenix1990@users.noreply.github.com>
2021-07-12 20:38:04 +0100

    Trying to show each image in console

--- module-2/final_project/start/Scripts/scripts.js
+++ module-2/final_project/start/Scripts/scripts.js
@@ -1,12 +1,22 @@
 let arrButtons = document.querySelectorAll('.filterButton')
 //console.log(buttons);
+let arrImages = document.querySelectorAll('.animalImage');
+//console.log('arrImages');
 
 for(counter=0; counter < arrButtons.length; counter++){
     let currentButton = arrButtons[counter];
-    console.log(currentButton);
+    //console.log(currentButton);
     currentButton.addEventListener('click',showHideImages)
 }
 
 function showHideImages(event){
-        console.log(event);
+    let animalFilter = event.target.getAttribute('animal');
+        console.log(animalFilter);
+        for(counter=0; counter < arrImages.length; counter++){
+            let currentImage = arrImages[counter];
+            console.log(currentImage);
+            
+            
+        }
+    
 }
